ab9de3e700da2ee3f34ee3a17a69865a77bcc590
Siedah <52298100+Siedahh@users.noreply.github.com>
2022-10-01 14:10:05 -0400

    HW3 update

--- HW3.py
+++ HW3.py
@@ -13,7 +13,7 @@ q1,q2,x,y = 1,-1,10,0
 
 # Electric potential
 def E_potential(q1,q2,x,y):
-    e0 = 1                                                      # permitivitty of free space, can use scipy.constants instead
+    e0 = 1                                                      # permitivitty of free space set to 1
     r1 = np.sqrt((x+5)**2+y**2)                                 # position of +C
     r2 = np.sqrt((x-5)**2+y**2)                                 # position of -C
     return (q1)/(4*np.pi*e0*(r1)) + (q2)/(4*np.pi*e0*(r2))      # sum of potentials
@@ -29,15 +29,21 @@ E_pot_map = []
 for j in np.linspace(ay,by,cy):
     row=[]
     for i in np.linspace(ax,bx,cx):
-        row.append(E_potential(1,-1,i,j))
+        row.append(E_potential(q1,q2,i,j))
     E_pot_map.append(row)
 E_pot_map = np.array(E_pot_map)
 
-# Visualization of potential in 1m x 1m grid
-#fig,axis = plt.subplots(1,2,figsize = (12,6))
-plt.imshow(E_pot_map, origin = 'lower', cmap = 'plasma',extent=[-50,50,-50,50])     # can also use 'RdBu'
-plt.colorbar()      #(ax=axis[0])                                                   # norm=colors.LogNorm((vmin=E_pot_map.min(), vmax=E_pot_map.max()))
-plt.clim(-0.005,0.005)
+############## Visualization of potential in 1m x 1m grid
+
+# discrete color values for image
+bounds=np.array([-1,-2e-2,-1e-3,-4e-4,-1e-4,1e-4,4e-4,1e-3,2e-2,1])
+norm = colors.BoundaryNorm(bounds,275)
+
+plt.imshow(E_pot_map, origin = 'lower',cmap = 'RdBu_r',extent=[ax/2,-ax/2,ay/2,-ay/2],norm = norm)
+clb = plt.colorbar(format='%.0e')
+plt.clim(bounds[0],bounds[bounds.size-1])
+clb.set_label('Electric Potential')
+plt.title('Electric Potential of a Positive and Negative Charge')
 plt.xlabel(' x position (cm)')
 plt.ylabel(' y position (cm)')
 plt.show()
@@ -59,39 +65,29 @@ def central_diff_2D(a,b,c):
         row_x = []
         row_y = []
         for i in np.linspace(ax,bx,cx):
-            row_x.append((E_potential(1,-1,i+h/2,j)-E_potential(1,-1,i-h/2,j))/(-h))
-            row_y.append((E_potential(1,-1,i,j+h/2)-E_potential(1,-1,i,j-h/2))/(-h))
+            row_x.append((E_potential(q1,q2,i+h/2,j)-E_potential(q1,q2,i-h/2,j))/(-h))
+            row_y.append((E_potential(q1,q2,i,j+h/2)-E_potential(q1,q2,i,j-h/2))/(-h))
         ddx.append(row_x)
         ddy.append(row_y)
     return np.array(ddx), np.array(ddy)
 
 
-# Electric field (can redefine E to be every fifth position for visualization purposes)
+# Electric field
 Ex, Ey = central_diff_2D(ax,ay,cy)
 
 
-# Visualization
-#Q = [(-5, 0), (5, 0)]                                                                           # Charges (x-cord, y-cord)
-#for q in Q:                                                                                     # charge locations
-#    plt.text(q[0], q[1], 'o', color = 'y', fontsize=15, va='center', ha='center')
-#    plt.text(q[0], q[1], 'o', color = 'y', fontsize=15, va='center', ha='center')
+############# Visualization of Electric Field
 
+# positive and negative charge labels
 plt.text(-5, 0, '+', color = 'r', fontsize=15, va='center', ha='center', weight='bold')
-plt.text(5,0, '-', color = 'r', fontsize=15, va='center', ha='center', weight='bold')
+plt.text(5,0, '-', color = 'b', fontsize=15, va='center', ha='center', weight='bold')
 
-# Streamplot
-color = np.log(np.sqrt(Ex**2 + Ey**2))                                                          # changes the color of the vector lines
+color = np.log(np.sqrt(Ex**2 + Ey**2))                              # gradient color for E field
 X,Y = np.meshgrid(np.linspace(ax,bx,cy),np.linspace(ay,by,cy))
-plt.streamplot(X,Y,Ex,Ey,color=color,density=1.2,cmap ='plasma')
-
-# Quiver plot
-#plt.colorbar(axis[1])
-#plt.quiver(X,Y,Ex,Ey,scale=3)
-
-# Draw circles for charges
-#charge1 = plt.Circle((-5,0), 0.05, color = '#FF0000')
-#charge2 = plt.Circle((5,0), 0.05, color = '#0000FF')
-#artist(charge1)
-
-plt.xlabel(' x position (cm)')
+plt.streamplot(X,Y,Ex,Ey,color=color,density=1.3,cmap ='Greys')     # Streamplot
+clb=plt.colorbar()
+clb.set_label('Electric Field')
+plt.title('Electric Field of a Positive and Negative Charge')
+plt.xlabel('x position (cm)')
+plt.ylabel('y position (cm)')
 plt.show()
